29b24fa9405485301f41d0c29164de56e50d0216
xy1137030414 <73565715+xy1137030414@users.noreply.github.com>
2025-07-24 07:35:53 +0000

    Deploying to gh-pages from @ xy1137030414/xy1137030414.github.io@1b1d2a481955bfd6f4eb692c2c35a94703b70d6e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%78%79%31%31%33%37%30%33%30%34%31%34@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
